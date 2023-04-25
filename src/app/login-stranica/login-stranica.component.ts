a18cc7dd8da006d0bbc71e0e51330a4794934d70
SRGT-STUDENTBJ1\Student1 <filip.pavlis.0@gmail.com>
2023-04-25 12:50:12 +0200

    Search bar centriran i započeta radnja s registracijom

--- src/app/login-stranica/login-stranica.component.ts
+++ src/app/login-stranica/login-stranica.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/shared/Login.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { Login } from 'src/app/shared/Login.model';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login-stranica',
   templateUrl: './login-stranica.component.html',
   styleUrls: ['./login-stranica.component.css']
 })
-export class LoginStranicaComponent {
+export class LoginStranicaComponent implements OnInit{
+
+  constructor(public service:LoginService,
+    private toastr:ToastrService) {}
+
+    ngOnInit(): void {
+      
+    }
+
   registerACT(){
     var x = document.getElementById("loginID") as HTMLDivElement;
     var y = document.getElementById("registerID") as HTMLDivElement;
@@ -24,5 +38,20 @@ export class LoginStranicaComponent {
     x.style.left = "50px";
     y.style.left = "450px";
     z.style.left = "0px";
+  }
+
+  onSubmit(form:NgForm){
+    this.service.postLogins().subscribe(
+      res =>{
+        this.resetForm(form);
+        this.toastr.success('Submitted successfully','Inspection Detail');
+      },
+      err => {console.log(err); }
+    );
+  }
+
+  resetForm(form:NgForm){
+    form.form.reset();
+    this.service.formData = new Login();
   }
 }
