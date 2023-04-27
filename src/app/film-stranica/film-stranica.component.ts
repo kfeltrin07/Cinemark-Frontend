a0f535c9fabd233726f2d3bc5f9e2d0f278ebf47
SRGT-STUDENTBJ1\Student1 <filip.pavlis.0@gmail.com>
2023-04-27 12:01:36 +0200

    Select Movie By Title Works

--- src/app/film-stranica/film-stranica.component.ts
+++ src/app/film-stranica/film-stranica.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Films } from '../shared/films.model';
+import { FilmsService } from '../shared/films.service';
 
 @Component({
   selector: 'app-film-stranica',
   templateUrl: './film-stranica.component.html',
   styleUrls: ['./film-stranica.component.css']
 })
-export class FilmStranicaComponent {
+export class FilmStranicaComponent implements OnInit {
+  
+  
+  constructor(public service:FilmsService) {
+  }
 
+  ngOnInit(): void {
+    this.changeToFilm();
+  }
+  selectedFilm:Films;
+
+  changeToFilm(){
+    this.selectedFilm = this.service.getFilmByName();
+  }
 }
--- src/app/shared/films.service.ts
+++ src/app/shared/films.service.ts
@@ -13,10 +13,26 @@ export class FilmsService {
   readonly baseURL = 'https://localhost:7168/api/Films'
 
   list : Films[];
+  selectedFilm:Films;
 
   getFilms(){
     this.http.get(this.baseURL).toPromise().then(
       res => this.list = res as Films[]);
+  }
+
+  updateFilmByName(filmName:string){
+    this.http.get(this.baseURL).toPromise().then(
+      res => this.list = res as Films[]);
+
+      for(var film of this.list){
+        if(film.title == filmName){
+          this.selectedFilm = film;
+        }
+      }
+  }
+
+  getFilmByName(){
+    return this.selectedFilm;
   }
 
 }
