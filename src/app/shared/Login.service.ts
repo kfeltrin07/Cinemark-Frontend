35107705ae59aa71cf31d5b855a7414b68813bd3
Tony Mostovac <tmostovac.ing@gmail.com>
2023-05-02 13:10:09 +0000

    change-environment-import

--- src/app/shared/Login.service.ts
+++ src/app/shared/Login.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Router, Routes } from '@angular/router';
 import { Login } from './Login.model';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { environment } from 'src/environments/environment.development';
+import { environment } from 'src/environmentsenvironment';
 import { Observable } from 'rxjs';
 
 
--- src/app/shared/films.service.ts
+++ src/app/shared/films.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http"
 import { Films } from './films.model';
-import { environment } from 'src/environments/environment.development';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
