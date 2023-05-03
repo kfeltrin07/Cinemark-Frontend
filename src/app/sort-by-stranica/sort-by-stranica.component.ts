41d7578880fda776c59dd12d6bb1e4eab00ac59c
Marko Žuna <mzuna@vub.hr>
2023-05-03 08:50:17 +0200

    Dizajniranje Sort By stranice - work in progress

--- src/app/sort-by-stranica/sort-by-stranica.component.ts
+++ src/app/sort-by-stranica/sort-by-stranica.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FilmsService } from '../shared/films.service';
+import { Films } from '../shared/films.model';
+import { RouterLink } from '@angular/router';
+import { FilmStranicaComponent } from '../film-stranica/film-stranica.component';
 
 @Component({
   selector: 'app-sort-by-stranica',
   templateUrl: './sort-by-stranica.component.html',
   styleUrls: ['./sort-by-stranica.component.css']
 })
-export class SortByStranicaComponent {
+export class SortByStranicaComponent implements OnInit {
 
+  selectedFilm:Films;
+  constructor(public service:FilmsService) {}
+
+  ngOnInit(): void {
+    this.service.getFilms();
+  }
+ 
+  updateSelectedFilm(film:string){
+    this.service.updateFilmByName(film);
+  }
+
+  openInfo(){
+    let filmBox = document.getElementById("filmBox") as HTMLDivElement;
+    filmBox.classList.remove("hide-details");
+  }
 }
