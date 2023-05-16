c4d0851f9c89383129d6333b1fc40ad26b26912c
Karlo Feltrin <kfeltrin@vub.hr>
2023-05-16 14:15:25 +0200

    Revert "Revert "movie managment je sada protected""
    This reverts commit b50385d5457bde9cb85d5748fb47ab6c688722ce.

--- src/app/movie-management-stranica/movie-management-stranica.component.ts
+++ src/app/movie-management-stranica/movie-management-stranica.component.ts
@@ -9,6 +9,9 @@ import { FilmsService } from '../_shared/films.service';
 import { GenreService } from '../_shared/genre.service';
 import { Film_Genre } from '../_shared/film_genre.model';
 import { Genres } from '../_shared/genre.model';
+import { UserStoreService } from '../_services/user-store.service';
+import { LoginService } from '../_shared/Login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-movie-management-stranica',
@@ -17,6 +20,7 @@ import { Genres } from '../_shared/genre.model';
 })
 export class MovieManagementStranicaComponent {
 
+  public role$:string="";
   film:Films = new Films();
   filmList:Films[];
   genreList:Genres[];
@@ -25,8 +29,17 @@ export class MovieManagementStranicaComponent {
   readonly baseURL = environment.baseURL+'api/Films'
   readonly baseURL2 = environment.baseURL + 'api/Film_Genre'
   
-  constructor(public storageService:StorageService, private toastr:ToastrService, private http:HttpClient, public filmService:FilmsService, public genreService:GenreService) {
+  constructor(public storageService:StorageService, private toastr:ToastrService, private http:HttpClient, public filmService:FilmsService, 
+    public genreService:GenreService, private userstore:UserStoreService, private loginService:LoginService, private router:Router) {
 
+    this.userstore.getRoleFromStore()
+  .subscribe(val=>{
+    let RoleFromToken=this.loginService.getRoleFromToken();
+    this.role$=val||RoleFromToken
+  })
+    if(this.role$!='superadmin'){
+      this.router.navigate(['']);
+    }
     this.refreshList();
     this.getGenres();
   }
