e024d5a6a9544f02551d3d43eab37ffd4c572df4
Filip Pavliš <filip.pavlis.0@gmail.com>
2023-05-16 13:57:44 +0200

    Super admin delete genre

--- src/app/movie-management-stranica/movie-management-stranica.component.ts
+++ src/app/movie-management-stranica/movie-management-stranica.component.ts
@@ -6,6 +6,9 @@ import { Films } from '../_shared/films.model';
 import { FormBuilder, FormGroup, FormsModule, NgForm, ReactiveFormsModule, FormControl } from '@angular/forms';
 import { environment } from 'src/environments/environment';
 import { FilmsService } from '../_shared/films.service';
+import { GenreService } from '../_shared/genre.service';
+import { Film_Genre } from '../_shared/film_genre.model';
+import { Genres } from '../_shared/genre.model';
 
 @Component({
   selector: 'app-movie-management-stranica',
@@ -16,11 +19,16 @@ export class MovieManagementStranicaComponent {
 
   film:Films = new Films();
   filmList:Films[];
+  genreList:Genres[];
+  filmGenreList:Film_Genre[];
+  idDelete:number;
   readonly baseURL = environment.baseURL+'api/Films'
+  readonly baseURL2 = environment.baseURL + 'api/Film_Genre'
   
-  constructor(public storageService:StorageService, private toastr:ToastrService, private http:HttpClient, public filmService:FilmsService) {
+  constructor(public storageService:StorageService, private toastr:ToastrService, private http:HttpClient, public filmService:FilmsService, public genreService:GenreService) {
 
     this.refreshList();
+    this.getGenres();
   }
 
   onSubmit(form:NgForm){
@@ -31,6 +39,23 @@ export class MovieManagementStranicaComponent {
 
   populateForm(selectedFilm:Films){
     this.film = Object.assign({},selectedFilm);
+    this.getGenres();
+
+    this.genreList = [];
+    this.filmGenreList = [];
+    const list = this.storageService.getFilmGenres();
+    const list2 = this.storageService.getGenres();
+
+    for(var filmGenre of list){
+      if(filmGenre.id_film == this.film.id_film){
+        this.filmGenreList.push(filmGenre);
+        for(var genre of list2){
+          if(genre.id_genre == filmGenre.id_genre){
+            this.genreList.push(genre);
+          }
+        }
+      }
+    }
   }
 
   refreshList(){
@@ -47,5 +72,43 @@ export class MovieManagementStranicaComponent {
     this.http.put(`${this.baseURL}/${this.film.id_film}`,this.film).subscribe();
     this.toastr.info("All changes saved.","Updated!")
     this.refreshList();
+  }
+
+  getGenres(){
+    this.genreService.GetFilmGenre();
+    this.genreService.GetGenres();
+  }
+
+  deleteGenre(selectedGenre:Genres){
+    console.log(selectedGenre);
+    console.log(this.filmGenreList);
+
+    for(var filmGenre of this.filmGenreList){
+      if(filmGenre.id_genre == selectedGenre.id_genre){
+        this.idDelete = filmGenre.id_field;
+      }
+    }
+    console.log(this.idDelete);
+    this.http.delete(`${this.baseURL2}/${this.idDelete}`).subscribe();
+    this.toastr.error("Genre deleted from the movie.","Deleted!")
+  }
+
+  refreshGenres(){
+    this.getGenres();
+    this.genreList = [];
+    this.filmGenreList = [];
+    const list = this.storageService.getFilmGenres();
+    const list2 = this.storageService.getGenres();
+
+    for(var filmGenre of list){
+      if(filmGenre.id_film == this.film.id_film){
+        this.filmGenreList.push(filmGenre);
+        for(var genre of list2){
+          if(genre.id_genre == filmGenre.id_genre){
+            this.genreList.push(genre);
+          }
+        }
+      }
+    }
   }
 }
